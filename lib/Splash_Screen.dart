850fce8f3d4ce64f7a7ca3d3ffadf8f2095ba201
1shabbirhussain <001.shabbirhussain@gmail.com>
2024-01-29 10:36:52 +0500

    added

--- lib/Splash_Screen.dart
+++ lib/Splash_Screen.dart
@@ -1,6 +1,4 @@
 import 'dart:async';
-import 'dart:ui';
-import "package:flutter/cupertino.dart";
 import 'package:flutter/material.dart';
 import 'package:login_page/Login.dart';
 
@@ -15,9 +13,9 @@ class _splashscreenState extends State<splashscreen> {
   @override
   void initState() {
     super.initState();
-    Timer(Duration(seconds: 3), () {
+    Timer(const Duration(seconds: 3), () {
       Navigator.pushReplacement(
-          context, MaterialPageRoute(builder: (context) => LoginPage()));
+          context, MaterialPageRoute(builder: (context) => const LoginPage()));
     });
   }
 
